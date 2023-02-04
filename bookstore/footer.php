9694ade5fe17b3d621ae96569b2fc6f039d653ea
AsankaRukshanRathnayake <88618141+AsankaRukshanRathnayake@users.noreply.github.com>
2023-02-04 11:51:07 +0530

    Add files via upload

---
+++ bookstore/footer.php
@@ -0,0 +1,30 @@
+<section>
+    <br><br><br>
+    
+    <div>
+        <h3>Quick Links</h3>
+
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="shop.php">Shop</a>
+        <a href="contact.php">Contact Us</a>
+    </div>
+
+    <div>
+        <h3>Extra Links</h3>
+        
+        <a href="signin.php">Login</a>
+        <a href="signup.php">Register</a>
+        <a href="cart.php">My Cart</a>
+        <a href="orders.php">Order info</a>
+    </div>
+
+    <div>
+        <h3>Contact Info</h3>
+
+        <a href="">Facebook</a>
+        <a href="">Twitter</a>
+        <a href="">LinkedIn</a>
+        <a href="">Instagram</a>
+    </div>
+</section>
