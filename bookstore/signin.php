30862920a6f2fb11d0145b93bf7e6a15219f2e4c
AsankaRukshanRathnayake <88618141+AsankaRukshanRathnayake@users.noreply.github.com>
2023-08-06 23:57:46 +0530

    Modified signin page

--- bookstore/signin.php
+++ bookstore/signin.php
@@ -1,43 +1,101 @@
-<!DOCTYPE html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, intial-scale=1.0">
-
-        <title>Sign-In</title>
-
-    </head>
-    <body>
-        <div>
-            <?php
-            include "header.php"
-            ?>
-        </div>
+<?php include "./cookie.php";
 
 
-        <div>
-            <form name="signin-form" action="" method="POST">
-                <h3>Sign In</h3>
+if (isset($_REQUEST['signin-btn'])) {
 
-                <label for="email">Email</label><br>
-                <input type="email" name="email" id="email" placeholder="Enter your email" required><br><br>
+    include "dbconnection.php";
+    $email = $_REQUEST['email'];
+    $pswd = $_REQUEST['pswd'];
+    $sql15 = "SELECT id FROM user WHERE pass ='$pswd' AND mail ='$email' ";
+    
+    $stmt = $con->prepare($sql15);
+    $stmt->execute();
+    $result = $stmt->fetchall();
+    if ($result) {
+        $_SESSION["useremail"] = $email;
+        $_SESSION["pass"] = $pswd;
+        setcookie("usr_id", $result[0]["id"], time() + (86400 * 30 ), "/");
 
-                <label for="password">Password</label><br>
-                <input type="password" name="password" id="password" placeholder="Enter your password" required><br><br>
-
-                <input type="submit" name="submit" value="Sign-In"><br>
-
-                <p>Don't have an Account? <a href="signup.php">Create Account</a></p>
-
-
-            </form>
-        </div>
-
-        <div>
-            <?php
-                include "footer.php"
-            ?>
-        </div>
+        echo '<script>window.location.replace("./index.php");</script>';
+        die();
+    } else {
         
-    </body>
-</html>
+        echo "<script>alert('Invalid login details!')</script>";
+    }
+}
+
+if (isset($_REQUEST["validate"])) {
+    if (isset($_COOKIE["usr_id"])) {
+       // var_dump($_COOKIE["usr_id"]);
+        include "./dbconnection.php";
+        $uid = $_COOKIE["usr_id"];
+        include "./sessions.php";
+        $sql15 = 'SELECT * FROM user where id="$uid"';
+        $stmt15 = $con->prepare($sql15);
+        $stmt15->execute();
+        $result = $stmt15->fetchAll();
+        if (
+            $stmt15->rowcount()
+            > 0
+        ) {
+            $_SESSION["useremail"] = $result["mail"];
+            $_SESSION["pass"] = $result["pass"];
+        }
+
+
+        echo '<script>window.location.replace("./index.php");</script>';
+        die();
+    }
+}
+if (isset($_COOKIE["usr_id"])) {
+   // var_dump($_COOKIE["usr_id"]);
+
+    echo '<script>window.location.replace("./udasd/dashboard.php");</script>';
+    die();
+}
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Page</title>
+
+    <!--
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+-->
+    <link rel="stylesheet" href="signin.css">
+
+</head>
+
+<body>
+    <div class="login-form-container">
+        <form action="./signin.php" method="POST">
+            <h3>sign in</h3>
+            <span>username</span>
+            <input type="email" name="email" class="box" placeholder="enter your email" id="">
+            <span>password</span>
+            <input type="password" name="pswd" class="box" placeholder="enter your password" id="">
+            <div class="checkbox">
+                <input type="checkbox" name="" id="remember-me">
+                <label for="remember-me"> remember me</label>
+            </div>
+            <input type="submit" value="sign in" class="btn" name="signin-btn">
+            <p>forget password ? <a href="#">click here</a></p>
+            <p>don't have an account ? <a href="signup.php">create one</a></p>
+        </form>
+    </div>
+
+    <script src="js/signup.js"></script>
+    </div>
+
+    <script>
+        function signUpValidation() {
+            document.getElementById('logVal').innerHTML = alert("password and email not match !!!");
+        }
+    </script>
+</body>
