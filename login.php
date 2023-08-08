d97d89724929e9148ad64137155251a734c55093
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-08-08 23:46:43 +0530

    Modified the login page

--- login.php
+++ login.php
@@ -1,5 +1,41 @@
 <?php
     include 'connection.php';
+
+    session_start();
+
+    if(isset($_POST['submit'])){
+
+        $email = mysqli_real_escape_string($connection, $_POST['email']);
+        $pass = mysqli_real_escape_string($connection, md5($_POST['password']));
+
+        $select_users = mysqli_query($connection, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+        if(mysqli_num_rows($select_users) > 0){
+
+            $row = mysqli_fetch_assoc($select_users);
+
+        if($row['user_type'] == 'admin'){
+
+            $_SESSION['admin_name'] = $row['name'];
+            $_SESSION['admin_email'] = $row['email'];
+            $_SESSION['admin_id'] = $row['id'];
+            header('location:admin_page.php');
+
+        }elseif($row['user_type'] == 'user'){
+
+            $_SESSION['user_name'] = $row['name'];
+            $_SESSION['user_email'] = $row['email'];
+            $_SESSION['user_id'] = $row['id'];
+            header('location:index.php');
+
+        }
+
+        }else{
+            $message[] = 'incorrect email or password!';
+        }
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -27,7 +63,7 @@
 
     
     <?php
-        /*
+        
         if(isset($message)){
             foreach($message as $message){
                 echo '
@@ -38,17 +74,23 @@
                 ';
             }
         }
-        */
+        
     ?>
 
     <div class="login_form_container">
+        
+        <form action="" method="post">
+            <h3>LOGIN</h3>
 
-        <form name="loginform" action="" method="post">
-            <h3>login now</h3>
-            <input type="email" name="email" placeholder="enter your email" required class="box">
-            <input type="password" name="password" placeholder="enter your password" required class="box">
-            <input type="submit" name="submit" value="login now" class="btn">
-            <p>don't have an account? <a href="register.php">register now</a></p>
+            <input type="email" name="email" placeholder="enter your email" required class="loginfield">
+            <input type="password" name="password" placeholder="enter your password" required class="loginfield">
+
+            <input type="submit" name="submit" value="Login Now" class="loginbutton">
+
+            <p>
+                Don't have an account? <br>
+                <a href="register.php">Click here to Register</a>
+            </p>
         </form>
 
     </div>
