5119c297e4c45641828421c089496f09581aa922
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-08-15 23:41:14 +0530

    Added product items and session

--- shop.php
+++ shop.php
@@ -1,5 +1,31 @@
 <?php
     include 'connection.php';
+    
+    session_start();
+    $user_id = $_SESSION['user_id'];
+
+    if(isset($_POST['add_to_cart'])){
+        if(!isset($user_id)){
+            header('location:login.php');
+        }
+        else{
+            $product_name = $_POST['product_name'];
+            $product_price = $_POST['product_price'];
+            $product_image = $_POST['product_image'];
+            $product_quantity = $_POST['product_quantity'];
+     
+            $check_cart_numbers = mysqli_query($connection, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+     
+            if(mysqli_num_rows($check_cart_numbers) > 0){
+                $message[] = 'already added to cart!';
+            }else{
+                mysqli_query($connection, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+                $message[] = 'product added to cart!';
+            }
+
+        }
+     
+     }
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +42,7 @@
    <link type="text/css" rel="stylesheet" href="css/styles.css">
    <link type="text/css" rel="stylesheet" href="css/header.css">
    <link type="text/css" rel="stylesheet" href="css/footer.css">
+   <link type="text/css" rel="stylesheet" href="css/shop.css">
 
 </head>
 <body>
@@ -25,35 +52,37 @@
 
     <!--main body-->
     <div class="shopping_container">
-    <h1 class="title">latest products</h1>
+        <h1 class="title">latest products</h1>
 
-<div class="box-container">
+        <div class="products_container">
 
-    <?php
-        $select_products = mysqli_query($connection, "SELECT * FROM `products`") or die('query failed');
-        if(mysqli_num_rows($select_products) > 0){
-            while($fetch_products = mysqli_fetch_assoc($select_products)){
-    ?>
+            <?php
+                $select_products = mysqli_query($connection, "SELECT * FROM `products`") or die('query failed');
+                
+                if(mysqli_num_rows($select_products) > 0){
+                    while($fetch_products = mysqli_fetch_assoc($select_products)){
+            ?>
 
-    <form action="" method="post" class="box">
-        <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
-        <div class="name"><?php echo $fetch_products['name']; ?></div>
-        <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+            <form action="" method="post" class="product">
+                <img class="product_image" src="original/uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+                <div class="product_name"><?php echo $fetch_products['name']; ?></div>
+                <div class="product_price">$<?php echo $fetch_products['price']; ?>/-</div>
         
-        <input type="number" min="1" name="product_quantity" value="1" class="qty">
-        <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-        <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-        <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-        <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-    </form>
+                <input type="number" min="1" name="product_quantity" value="1" class="product_quantity">
+                <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+                <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+                <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+                <input type="submit" value="add to cart" name="add_to_cart" class="addtocart">
+            </form>
    
-    <?php
-        }
-    }else{
-      echo '<p class="empty">no products added yet!</p>';
-    }
-    ?>
+            <?php
+                    }
+                }else{
+                    echo '<p class="empty">no products added yet!</p>';
+                }
+            ?>
 
+        </div>
     </div>
 
     <!--footer-->
