7ace96826daae96b4d7be7588eb392adcbab55bf
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-08-18 21:23:31 +0530

    Added side navigation bar

---
+++ admin/sidenavbar.php
@@ -0,0 +1,11 @@
+<div class="admin_details_container">
+
+</div>
+
+<div class="quick_links_container">
+    <div class="admin_link"><a href="products.php">Add Products</a></div>
+    <div class="admin_link"><a href="orders.php">View Orders</a></div>
+    <div class="admin_link"><a href="messages.php">View Message</a></div>
+    <div class="admin_link"><a href="users.php">See User</a></div>
+ </div>
+
