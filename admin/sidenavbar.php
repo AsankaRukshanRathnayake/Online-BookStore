80cb7175c9b6aa6de03aebba3de1c053a6cec5ca
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-08-19 22:58:28 +0530

    Added viewing admin user details

--- admin/sidenavbar.php
+++ admin/sidenavbar.php
@@ -1,4 +1,9 @@
 <div class="admin_details_container">
+    <img src="../moreicons/admin.png" alt="">
+
+    <p>username : <span><?php echo $_SESSION['admin_name']; ?></span></p>
+    <p>email : <span><?php echo $_SESSION['admin_email']; ?></span></p>
+    <a href="logout.php" class="delete-btn">logout</a>
 
 </div>
 
@@ -7,5 +12,7 @@
     <div class="admin_link"><a href="orders.php">View Orders</a></div>
     <div class="admin_link"><a href="messages.php">View Message</a></div>
     <div class="admin_link"><a href="users.php">See User</a></div>
- </div>
+</div>
+
+
 
